a8d280cd4897472f43b084401b939edf90ac3888
KumagaeMayuko <mayukokumagae315@gmail.com>
2023-04-19 19:01:23 +0900

    追加修正

---
+++ manager/controller/test_success.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace manager\controller;
+require_once dirname( __FILE__, 3) . '/common/model/Bootstrap.class.php';
+
+use common\model\Bootstrap;
+use common\model\PDODatabase;
+use common\model\Category;
+use common\model\Login;
+use manager\model\manager;
+
+$loader = new \Twig_Loader_Filesystem( Bootstrap::TEMPLATE_DIR );
+$twig = new \Twig_Environment( $loader, [ 
+    'cache' => Bootstrap::CACHE_DIR
+] );
+$db = new PDODatabase(Bootstrap::DB_HOST, Bootstrap::DB_USER, Bootstrap::DB_PASS, Bootstrap::DB_NAME, Bootstrap::DB_TYPE);
+$ctg = new Category($db);
+$login = new Login;
+$manager = new manager;
+
+$context = [];
+
+$message = '作成完了しました';
+$url = "top.php";
+$url_message = 'トップページへ戻る';
+
+$context['message'] = $message;
+$context['url'] = $url;
+$context['url_message'] = $url_message;
+
+$template = $twig->loadTemplate('user/view/process_complete.html.twig');
+$template->display( $context );
